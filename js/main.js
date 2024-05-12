f07907255c4f07f409f4a7a5b23a904f560d5292
louison-rubeck <louison.rubeck@gmail.com>
2024-05-12 15:22:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,13 +27,38 @@ projectsCloseBtns.forEach((projectsCloseBtn) => {
     });
 });
 
-//Our clients - Swiper
-
 //Website dark/light theme
 
 //Scroll to top button
+const scrollToTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+    scrollToTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
 
 //Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+        }
+        else{
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+})
 
 //Responsive navigation menu toggle
 
